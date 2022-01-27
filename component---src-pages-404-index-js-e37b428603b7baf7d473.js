06e1576a478d35346ca261de27c276fbbc1c2c24
AliDa <info.alida.ir@gmail.com>
2022-01-27 15:01:41 +0330

    New Post

--- component---src-pages-404-index-js-e37b428603b7baf7d473.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[911],{3673:function(t,e,n){n.r(e);var u=n(7294),a=n(3751);e.default=function(){return u.createElement("div",null,u.createElement(a.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-index-js-e37b428603b7baf7d473.js.map
